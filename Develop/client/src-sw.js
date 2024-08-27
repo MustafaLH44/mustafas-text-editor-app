3ba8375b92329a7ddd2e84c9a7401223dfc7cf16
Mustafa Ahmed <musi1996@yahoo.com>
2024-08-26 20:58:44 -0700

    added page cacheing to sw

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -5,8 +5,10 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache the assets in the manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache page navigations (HTML) with CacheFirst strategy
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,7 +16,7 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
     }),
   ],
 });
@@ -24,7 +26,24 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+// Register a route to handle page navigations with the page cache strategy
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+registerRoute(
+  // Define the callback function to filter the requests you want to cache
+  ({ request }) => ['style', 'script', 'image'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60, // Cache a maximum of 60 entries
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+      }),
+    ],
+  })
+);
 registerRoute();
